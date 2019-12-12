eca1cd9662ae0e58c37494c6510ffa07b418bac1
lucaslims <57826357+lucaslims@users.noreply.github.com>
2019-12-11 21:07:42 -0300

    Revert "11/12/19 19:30"
    This reverts commit e000325c0898fdbf021f10cf039f3345b67f7518.

--- ClutchKinetcs/FormCadViagem.cs
+++ ClutchKinetcs/FormCadViagem.cs
@@ -19,10 +19,7 @@ namespace ClutchKinetcs
     {
         MySqlConnection connMySql;
         SqlConnection connSqlServer;
-        DataTable dt;
         clsGlobal varGlob = new clsGlobal();
-        clsConexao clsConn = new clsConexao();
-
 
         public FormCadViagem()
         {
@@ -36,100 +33,10 @@ namespace ClutchKinetcs
             connSqlServer = clsConexao.GetConexaoSqlServer();
 
         }
-        private void PreencherCombo(int _opc, ComboBox _comboBox)
-        {
-            try
-            {
-                string query = "";
-                connMySql = clsConexao.GetConexaoMySql();
-                connSqlServer = clsConexao.GetConexaoSqlServer();
 
-
-                varGlob.AbrirConexaoMySql(connMySql);
-                if (_opc == 4)
-                {
-                    query = "select NOME from TB_CD_PESSOA ORDER BY NOME;";
-                }
-                else if (_opc == 2)
-                {
-                    query = "select PLACA from TB_CD_VEICULO ORDER BY PLACA";
-                }
-              
-
-
-                dt = varGlob.RetornaDataTableMySql(query, connMySql);
-                _comboBox.Items.Clear();
-                foreach (DataRow row in dt.Rows)
-                {
-                    if (_opc != 4)
-                        _comboBox.Items.Add(row.Field<string>("PLACA"));
-                    else
-                        _comboBox.Items.Add(row.Field<string>("NOME"));
-
-                }
-
-                if (_comboBox.Items.Count > 0)
-                    _comboBox.SelectedIndex = 0;
-
-
-            }
-            catch (Exception ex) { MessageBox.Show("Erro: " + ex, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error); }
-            finally { varGlob.FecharConexaoMySql(connMySql); }
-            }
-       
-    
         private void button1_Click(object sender, EventArgs e)
         {
-            connMySql = clsConexao.GetConexaoMySql();
-            connSqlServer = clsConexao.GetConexaoSqlServer();
 
-            List<string> dadosViagem = new List<string>();
-            try
-            {
-
-                dadosViagem.Add(txtOrigemVia.Text);
-                dadosViagem.Add(txtDestino.Text);
-                dadosViagem.Add(txtCombustivel.Text);
-                dadosViagem.Add(cmbMotorista.Text);
-                dadosViagem.Add(cmbPlaca.Text);
-                dadosViagem.Add("1");
-
-
-
-
-
-                varGlob.InsereLog();
-
-                clsDalViagem dalviagem = new clsDalViagem();
-
-                connMySql.Open();
-                connSqlServer.Open();
-                try
-                {
-
-                    dalviagem.insertViagemProcedure(connMySql, connSqlServer, dadosViagem);
-
-                    connMySql.Close();
-                    connSqlServer.Close();
-                    MessageBox.Show("Cadastro realizado com sucesso!!");
-                }
-
-                catch (Exception ex) { MessageBox.Show("registro não encontrado! erro: " + ex.Message); }
-                finally
-                {
-                    connMySql.Close();
-                    connSqlServer.Close();
-                }
-            }
-            catch (Exception ex) { MessageBox.Show("registro não encontrado! erro: " + ex.Message); }
-
-        }
-
-        private void bunifuMetroTextbox1_OnValueChanged(object sender, EventArgs e)
-        {
-            PreencherCombo(4, cmbMotorista);
-            PreencherCombo(2, cmbPlaca);
-     
         }
     }
 }
